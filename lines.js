0667c4705a44fc675aa9e5b121ab2e45046c09a4
Yuanyuan Ruan <yruan2@dons.usfca.edu>
2017-05-07 20:26:35 -0700

    Add files via upload

--- lines.js
+++ lines.js
@@ -83,35 +83,31 @@ d3.csv("line.csv", function(error, data) {
                 .text(d.key);
 
 
-    // Add the X Axis
-    svg.append("g")
-        //.attr("class", "axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        ;
+                // Add the X Axis
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x).ticks(7).tickFormat(d3.format("")));
 
-    // text label for the x axis
-svg.append("text")
-    .attr("transform",
-          "translate(" + (width/2) + " ," +
-                         (height + margin.top + 3) + ")")
-    .style("text-anchor", "middle")
-    .text("Year");
+                // text label for the x axis
+            svg.append("text")
+              .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top + 3) + ")")
+              .style("text-anchor", "middle")
+              .text("Year");
 
-    // Add the Y Axis
-    svg.append("g")
-    //.attr("class", "axis")
-    .call(d3.axisLeft(y));
+              // Add the Y Axis
+            svg.append("g")
+            //.attr("class", "axis")
+              .call(d3.axisLeft(y));
 
 
-    // text label for the y axis
-  svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Count");
+              // text label for the y axis
+            svg.append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 0 - margin.left)
+              .attr("x",0 - (height / 2))
+              .attr("dy", "1em")
+              .style("text-anchor", "middle")
+              .text("Count");
 
 
 });
